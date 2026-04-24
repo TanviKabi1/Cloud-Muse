71b7739707866becde31a1e7eb8efbc2775c0dda
Tanvi Kabi <tanvi.kabi@gmail.com>
2026-04-25 02:18:44 +0530

    Fix base path for GitHub pages

--- app/frontend/vite.config.js
+++ app/frontend/vite.config.js
@@ -5,7 +5,7 @@ import path from 'path'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  base: "/CloudMuse/",
+  base: "/Cloud-Muse/",
   plugins: [react(), tailwindcss()],
   resolve: {
     alias: {
